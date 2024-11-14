61a482fdc0d885dad717c447a9d1cdb08f45e2c7
GTG671L9R2 <gtg671l9r2@GTG671L9R2ui-MacBookPro.local>
2024-11-14 15:48:16 +0900

    feat: tokens 패키지 완성

--- packages/tokens/dist/color/palette/common.d.ts
+++
@@ -1,3 +0,0 @@
-type BlackAndWhite = '$black950' | '$black900' | '$blackAlpha200' | '$blackAlpha100' | '$blackAlpha50' | '$white0' | '$whiteAlpha200' | '$whiteAlpha100' | '$whiteAlpha50';
-export declare const blackAndWhitePalette: Record<BlackAndWhite, string>;
-export {};
--- packages/tokens/dist/shadow.d.ts
+++
@@ -1,8 +0,0 @@
-export declare const Shadow: {
-    $shadow1: string;
-    $shadow2: string;
-    $shadow3: string;
-    $shadow4: string;
-    $shadow5: string;
-    $shadow6: string;
-};
